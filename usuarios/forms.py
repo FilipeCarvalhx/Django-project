5d07693c4aebc9f3cbbd4d69e41c333c955bab72
Filipe <filipe.dev47@gmail.com>
2025-07-11 17:02:46 -0300

    Formulario de cadastro de pessoas sendo estilizado

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -5,3 +5,19 @@ class PersonForm(forms.ModelForm):
     class Meta:
         model = Usuario
         fields = ['nome', 'idade', 'email']
+
+        widgets = {
+                'nome': forms.TextInput(attrs={
+                    'placeholder': 'Seu nome',
+                    'class': 'form-input'
+                }),
+                'idade': forms.TextInput(attrs={
+                    'placeholder': 'Idade',
+                    'class': 'form-input'
+                }),
+                'email': forms.EmailInput(attrs={
+                    'placeholder': 'Seu e-mail',
+                    'class': 'form-input'
+                }),
+                
+            }
